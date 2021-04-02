a71220bbd3546c0e1353733cf99b27dc170f166f
CuasurRigoder <suotian1965@126.com>
2021-04-02 15:45:56 +0800

    234234

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+// 增加url 依赖
+var urllib = require('url');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// get请求
+router.get('/index', function (req, res, next) {
+
+	// 初始数据
+	var data = {
+	    status: '100', 
+	    msg: '操作成功',
+	    data: {
+	        userId: '123456',
+	        userName: 'hgdqstudio',
+	        blog: 'http://hgdqstudio.online'
+	    }
+	};
+	
+    var params = urllib.parse(req.url, true);
+    var query2 = params.query;
+    // 打印get请求中的接口参数
+    console.log(query2);
+    res.end(JSON.stringify(data));
+});
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// 登录接口
+router.post('/login',function (req, res, next) {
+
+	var dataSuccess = {
+	    status: '100', 
+	    msg: '登录成功',
+	    data: {
+	        userId: '20170113',
+	        userName: 'hgdqstudio',
+	        blog: 'http://hgdqstudio.online'
+	    }
+	};
+	var dataError = {
+	    status: '99', 
+	    msg: '用户名或密码错误'
+	};
+
+    // 打印post请求的数据内容
+    console.log(req.body);
+    console.log(req.body.username);
+    console.log(req.body.password);
+    if (req.body.username == "qwe" && req.body.password == "123") {
+        res.end(JSON.stringify(dataSuccess));
+    } else {
+        res.end(JSON.stringify(dataError));
+    }
+});
+
 module.exports = router;
