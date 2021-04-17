40e0114e1417942652886b4e220bee9cae9f66a2
CuasurRigoder <suotian1965@126.com>
2021-04-17 12:04:18 +0800

    234234

--- client/src/router/router.js
+++ client/src/router/router.js
@@ -5,9 +5,28 @@ import Index from '@/components/index.vue'
 
 Vue.use(Router)
 
+// 复用组件时，想对路由参数的变化作出响应的话，你可以简单地 watch（监测变化） $route 对象
 Index.watch = {
   '$route' (to, from) {
   }
+}
+
+// 在渲染该组件的对应路由被 confirm 前调用
+// 不！能！获取组件实例 `this`
+// 因为当钩子执行前，组件实例还没被创建
+Index.beforeRouteEnter = (to, from, next) => {
+}
+
+// 在当前路由改变，但是该组件被复用时调用
+// 举例来说，对于一个带有动态参数的路径 /foo/:id，在 /foo/1 和 /foo/2 之间跳转的时候，
+// 由于会渲染同样的 Foo 组件，因此组件实例会被复用。而这个钩子就会在这个情况下被调用。
+// 可以访问组件实例 `this`
+Index.beforeRouteUpdate = (to, from, next) => {
+}
+
+// 导航离开该组件的对应路由时调用
+// 可以访问组件实例 `this`
+Index.beforeRouteLeave = (to, from, next) => {
 }
 
 export default new Router({
@@ -26,6 +45,10 @@ export default new Router({
       // }
       name: 'index',
       component: Index,
+      // 路由独享钩子
+      beforeEnter: (to, from, next) => {
+        next()
+      },
       children: [
         {
           // 当 /index/:id/profile 匹配成功，
@@ -43,5 +66,12 @@ export default new Router({
       name: 'hello',
       component: Hello
     }
-  ]
+  ],
+  // 全局路由钩子（之后执行）
+  beforeEach: (to, from, next) => {
+
+  },
+  // 全局钩子（之后执行）
+  afterEach: () => {
+  }
 })
