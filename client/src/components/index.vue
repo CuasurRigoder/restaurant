502ea896d7cf9d9eee135076b2e6db73c03e1099
CuasurRigoder <suotian1965@126.com>
2021-04-17 15:44:29 +0800

    234234

--- client/src/components/index.vue
+++ client/src/components/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <transition name="slide-right">
+  <transition name="fade">
     <div class="index">
       <h1>{{ msg }}</h1>
       <!-- 映射路由的 id -->
@@ -21,11 +21,33 @@ export default {
     return {
       msg: 'index.vue'
     }
+  },
+  beforeRouteEnter (to, from, next) {
+    next(true)
+  },
+  // 组建创建完成之后执行
+  created () {
+    // 组件创建完后获取数据，
+    // 此时 data 已经被 observed 了
+    this.fetchData()
+  },
+  // 监听路由变化
+  watch: {
+    // 如果路由有变化，会再次执行该方法
+    '$route': 'fetchData'
+  },
+  methods: {
+    fetchData () {
+      setTimeout(() => {
+        this.msg = 'index.vue fetchData()已经被执行过'
+      }, 1000)
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- scoped 组建私有样式 -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
@@ -43,5 +65,12 @@ li {
 
 a {
   color: #42b983;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 1s
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+  opacity: 0
 }
 </style>
