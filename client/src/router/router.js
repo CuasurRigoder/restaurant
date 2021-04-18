d12e7da22fa97a7f72f6f209e35fbc7ea13370aa
CuasurRigoder <suotian1965@126.com>
2021-04-18 14:53:40 +0800

    234234

--- client/src/router/router.js
+++ client/src/router/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello.vue'
-import Index from '@/components/index.vue'
+import Hello from '@/components/view/Hello.vue'
+import Index from '@/components/view/index.vue'
 
 Vue.use(Router)
 
